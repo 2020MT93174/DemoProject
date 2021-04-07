48e293e69054c0cd479ef0e70c9b0d97aaec638e
ayushiVerma <ayushiver@gmail.com>
2021-04-07 10:33:10 +0530

    updated support text

--- src/com/bits/devops/feature1.java
+++ src/com/bits/devops/feature1.java
@@ -5,7 +5,7 @@ public class Feature1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("This is my Java project");
-	System.out.println("This is for feature1 for demo java project");
+	System.out.println("This is for feature1 for testing demo java project.");
 
 	}
 
