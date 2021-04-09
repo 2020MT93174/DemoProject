3a4a2a0c0d8935d73b2f9e037df574f791d6ef8d
ayushiVerma <ayushiver@gmail.com>
2021-04-09 20:52:17 +0530

    added lined in feature3

--- src/com/bits/devops/feature3.java
+++ src/com/bits/devops/feature3.java
@@ -5,6 +5,10 @@ public class Feature3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Line #1 pushed");
+		System.out.println("Line #2 pushed");
+		System.out.println("Line #3 pushed");
+		System.out.println("Line #4 pushed");
 	}
+
 
 }
