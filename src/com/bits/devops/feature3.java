17c5b20fab7e60ccf9e04ace9b9c47d434ad0e2b
ayushiVerma <ayushiver@gmail.com>
2021-04-10 17:34:08 +0530

    added new feature enhancement

--- src/com/bits/devops/feature3.java
+++ src/com/bits/devops/feature3.java
@@ -4,11 +4,7 @@ public class Feature3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Line #1 pushed");
-		System.out.println("Line #2 pushed");
-		System.out.println("Line #3 pushed");
-		System.out.println("Line #4 pushed");
+	System.out.println("This feature 3 file");
 	}
-
 
 }
